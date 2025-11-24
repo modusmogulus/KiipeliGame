1a5c114831cc2296b18843da15c84ff4be67b1d2
modusmogulus <vaingald@gmail.com>
2025-11-24 15:42:39 +0200

    sauvakavelija update

--- pasii/Scenes/Menus/mainmenu.gd
+++ pasii/Scenes/Menus/mainmenu.gd
@@ -3,36 +3,44 @@ var selected_node : Node
 var size_multiplier_on_select : Vector3 = Vector3(1.1, 1.1, 1.1)
 @export var cam : Camera3D
 @export var camtarget : Node3D
+@export var lerpstep : float = 0.1
+
 var original_cam_rotation : Vector3
 var original_cam_position : Vector3
 var selectables : Array[Node3D]
 var selection_index : int
+@export var upvector : Vector3 = Vector3.UP
 
 func _ready() -> void:
 	original_cam_rotation = cam.rotation_degrees
 	original_cam_position = cam.global_position
-
 	if selected_node == null:
 		camtarget.global_rotation = original_cam_rotation
 		camtarget.global_position = original_cam_position
 
 func _process(delta: float) -> void:
-	cam.global_position = camtarget.global_position
+	#cam.global_position = camtarget.global_position
+	#cam.global_rotation = camtarget.global_rotation
+	if Input.is_action_just_pressed("choice_up"):
+		select_menubutton(selection_index-1)
+	if Input.is_action_just_pressed("choice_down"):
+		select_menubutton(selection_index+1)
+	
 	if !(selected_node == null):
 		var _t = lerp(original_cam_position, selected_node.global_position, 0.5)
 		camtarget.global_position = lerp(camtarget.position, _t, 0.1)
-func select_menubutton(index : int):
-	for stb in selectables:
-		if selectables.find(stb) != index:
+		camtarget.look_at_from_position(camtarget.global_position, selected_node.global_position, upvector)
+		cam.global_position = lerp(cam.global_position, camtarget.global_position, lerpstep)
+		cam.global_rotation = lerp(cam.global_rotation, camtarget.global_rotation, lerpstep)
+		camtarget.global_position.y = original_cam_position.y
+func deselect_all():
+		for stb in selectables:
 			stb.deselect_self()
-	selection_index = wrap(index, -1, selectables.size())
+func select_menubutton(index : int):
+	deselect_all()
+	selection_index = wrap(index, 0, selectables.size())
 	#selection_index = index
 	selectables[selection_index].select_self()
 	
-func _input(event: InputEvent) -> void:
-	if Input.is_action_just_pressed("choice_up"):
-		select_menubutton(selection_index+1)
-		
-	if Input.is_action_just_pressed("choice_down"):
-		select_menubutton(selection_index-1)
+#func _input(event: InputEvent) -> void:
 	
--- pasii/Scenes/Menus/menubutton.gd
+++ pasii/Scenes/Menus/menubutton.gd
@@ -1,22 +1,30 @@
 extends StaticBody3D
 
 @export var menu : Node
+#@export var button_index : int
+var respond_to_mouse = false
+#NOTE: Enter tree is ordered from top down while ready is not
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+func _enter_tree() -> void:
 	menu.selectables.append(self)
-
+	#if menu.selectables.size() > button_index+1:
+	#	menu.selectables.resize(button_index+1)
+	#	menu.selectables[button_index] = self
 func select_self() -> void:
+	if menu.selected_node == self:	return
 	menu.selected_node = self
 	scale *= menu.size_multiplier_on_select
 	menu.selection_index = menu.selectables.find(self)
 func deselect_self() -> void:
+		if menu.selected_node != self:	return
 		scale /= menu.size_multiplier_on_select
-		#if menu.selected_node != self:	return
+		
 		menu.selected_node = null
 		#menu.selection_index = -1
 func _mouse_enter() -> void:
-	select_self()
+	if respond_to_mouse:
+		menu.deselect_all()
+		select_self()
 
 #func _mouse_exit() -> void:
 #	deselect_self()
